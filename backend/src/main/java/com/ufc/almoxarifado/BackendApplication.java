e263392d80fc2a15a32902a6ddc5dd3b3101f6f9
desou <desousafelipe50@gmail.com>
2026-03-25 15:49:59 -0300

    feat(app): inicializa backend da aplicação

---
+++ backend/src/main/java/com/ufc/almoxarifado/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.almoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
